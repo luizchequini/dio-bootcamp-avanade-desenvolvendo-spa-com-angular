ae0084e34e49752d442a6f621f41875becefa4df
Luiz Chequini <luizchequini@gmail.com>
2021-02-27 17:44:11 -0300

    Criando o primeiro componente e app-routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRouterModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    AppRouterModule,
     BrowserAnimationsModule
   ],
   providers: [],
